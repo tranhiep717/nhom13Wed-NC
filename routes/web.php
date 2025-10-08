d2fd160f110ae828b29b650310103ed5ff04024f
tranhiep717 <hieptran19102005@gmail.com>
2025-10-09 02:18:38 +0700

    cập nhật backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+Route::get('/store', [PageController::class, 'store']);
+Route::get('/product', [PageController::class, 'product']);
+Route::get('/checkout', [PageController::class, 'checkout']);
